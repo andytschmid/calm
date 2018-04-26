c38b2e46243f1655904863c99670c7638562da5a
Andy Schmid <andytschmid@gmail.com>
2018-04-25 21:08:18 -0400

    Add files via upload

---
+++ peaks/collateral/peakJson.js
@@ -0,0 +1,408 @@
+db.peaks.insert([
+{
+    rank: "1",
+    peak: "Mt. Elbert",
+    elevation: "14,433’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "2",
+    peak: "Mt. Massive",
+    elevation: "14,421’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "3",
+    peak: "Mt. Harvard",
+    elevation: "14,420’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "4",
+    peak: "Blanca Peak",
+    elevation: "14,345’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "5",
+    peak: "La Plata Peak",
+    elevation: "14,336’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "6",
+    peak: "Uncompahgre Peak",
+    elevation: "14,309’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "7",
+    peak: "Crestone Peak",
+    elevation: "14,294’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "8",
+    peak: "Mt. Lincoln",
+    elevation: "14,286’",
+    state: "Colorado",
+    range: "Mosquito Range"
+},
+{
+    rank: "9",
+    peak: "Grays Peak",
+    elevation: "14,270’",
+    state: "Colorado",
+    range: "Front Range"
+},
+{
+    rank: "10",
+    peak: "Mt. Antero",
+    elevation: "14,269’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "11",
+    peak: "Torreys Peak",
+    elevation: "14,267’",
+    state: "Colorado",
+    range: "Front Range"
+},
+{
+    rank: "12",
+    peak: "Castle Peak",
+    elevation: "14,265’",
+    state: "Colorado",
+    range: "Elk Mountains"
+},
+{
+    rank: "13",
+    peak: "Quandary Peak",
+    elevation: "14,265’",
+    state: "Colorado",
+    range: "Tenmile Range"
+},
+{
+    rank: "14",
+    peak: "Mt. Evans",
+    elevation: "14,264’",
+    state: "Colorado",
+    range: "Front Range"
+},
+{
+    rank: "15",
+    peak: "Longs Peak",
+    elevation: "14,255’",
+    state: "Colorado",
+    range: "Front Range"
+},
+{
+    rank: "16",
+    peak: "Mt. Wilson",
+    elevation: "14,246’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "*",
+    peak: "Mt. Cameron",
+    elevation: "14,238’",
+    state: "Colorado",
+    range: "Mosquito Range"
+},
+{
+    rank: "17",
+    peak: "Mt. Shavano",
+    elevation: "14,229’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "18",
+    peak: "Mt. Belford",
+    elevation: "14,197’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "19",
+    peak: "Crestone Needle",
+    elevation: "14,197’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "20",
+    peak: "Mt. Princeton",
+    elevation: "14,197’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "21",
+    peak: "Mt. Yale",
+    elevation: "14,196’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "22",
+    peak: "Mt. Bross",
+    elevation: "14,172’",
+    state: "Colorado",
+    range: "Mosquito Range"
+},
+{
+    rank: "23",
+    peak: "Kit Carson Peak",
+    elevation: "14,165’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "*",
+    peak: "El Diente Peak",
+    elevation: "14,159’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "24",
+    peak: "Maroon Peak",
+    elevation: "14,156’",
+    state: "Colorado",
+    range: "Elk Mountains"
+},
+{
+    rank: "25",
+    peak: "Tabeguache Peak",
+    elevation: "14,155’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "26",
+    peak: "Mt. Oxford",
+    elevation: "14,153’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "27",
+    peak: "Mt. Sneffels",
+    elevation: "14,150’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "28",
+    peak: "Mt. Democrat",
+    elevation: "14,148’",
+    state: "Colorado",
+    range: "Mosquito Range"
+},
+{
+    rank: "29",
+    peak: "Capitol Peak",
+    elevation: "14,130’",
+    state: "Colorado",
+    range: "Elk Mountains"
+},
+{
+    rank: "30",
+    peak: "Pikes Peak",
+    elevation: "14,110’",
+    state: "Colorado",
+    range: "Front Range"
+},
+{
+    rank: "31",
+    peak: "Snowmass Mountain",
+    elevation: "14,092’",
+    state: "Colorado",
+    range: "Elk Mountains"
+},
+{
+    rank: "32",
+    peak: "Mt. Eolus",
+    elevation: "14,083’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "33",
+    peak: "Windom Peak",
+    elevation: "14,082’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "34",
+    peak: "Challenger Point",
+    elevation: "14,081’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "35",
+    peak: "Mt. Columbia",
+    elevation: "14,073’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "36",
+    peak: "Missouri Mountain",
+    elevation: "14,067’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "37",
+    peak: "Humboldt Peak",
+    elevation: "14,064’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "38",
+    peak: "Mt. Bierstadt",
+    elevation: "14,060’",
+    state: "Colorado",
+    range: "Front Range"
+},
+{
+    rank: "*",
+    peak: "Conundrum Peak",
+    elevation: "14,060’",
+    state: "Colorado",
+    range: "Elk Mountains"
+},
+{
+    rank: "39",
+    peak: "Sunlight Peak",
+    elevation: "14,059’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "40",
+    peak: "Handies Peak",
+    elevation: "14,048’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "41",
+    peak: "Culebra Peak",
+    elevation: "14,047’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "42",
+    peak: "Ellingwood Point",
+    elevation: "14,042’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "43",
+    peak: "Mt. Lindsey",
+    elevation: "14,042’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "*",
+    peak: "North Eolus",
+    elevation: "14,039’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "44",
+    peak: "Little Bear Peak",
+    elevation: "14,037’",
+    state: "Colorado",
+    range: "Sangre de Cristo"
+},
+{
+    rank: "45",
+    peak: "Mt. Sherman",
+    elevation: "14,036’",
+    state: "Colorado",
+    range: "Mosquito Range"
+},
+{
+    rank: "46",
+    peak: "Redcloud Peak",
+    elevation: "14,034’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "47",
+    peak: "Pyramid Peak",
+    elevation: "14,018’",
+    state: "Colorado",
+    range: "Elk Mountains"
+},
+{
+    rank: "48",
+    peak: "Wilson Peak",
+    elevation: "14,017’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "49",
+    peak: "Wetterhorn Peak",
+    elevation: "14,015’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "50",
+    peak: "San Luis Peak",
+    elevation: "14,014’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+},
+{
+    rank: "*",
+    peak: "North Maroon Peak",
+    elevation: "14,014’",
+    state: "Colorado",
+    range: "Elk Mountains"
+},
+{
+    rank: "51",
+    peak: "Mt. of the Holy Cross",
+    elevation: "14,005’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "52",
+    peak: "Huron Peak",
+    elevation: "14,003’",
+    state: "Colorado",
+    range: "Sawatch Range"
+},
+{
+    rank: "53",
+    peak: "Sunshine Peak",
+    elevation: "14,001’",
+    state: "Colorado",
+    range: "San Juan Mountains"
+}
+])
